1e969410ea7de5f3603c39e25f0c6aa26786347f
Argimiro Crespo <acrespo123@yahoo.es>
2025-07-24 19:11:52 -0400

    Prueba de creacion de componentes

---
+++ src/components/MenuItems.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+</style>
